0bcc8810495d48b87bb96d7c35989c69b876155a
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-01-02 11:53:49 +0200

    dz

--- proxy.ts
+++ proxy.ts
@@ -4,8 +4,7 @@ import { checkSession } from '@/lib/api/serverApi';
 
 const privateRoutes = ['/profile', '/notes'];
 const authRoutes = ['/sign-in', '/sign-up'];
-
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const cookieStore = await cookies();
 
